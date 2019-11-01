78e63b280e9f039596bea4611bc11b992188e5dc
Stijn Nolet <stijnnolet@hotmail.com>
2019-11-01 16:44:07 +0100

    created geometry function

---
+++ geometry_function.m
@@ -0,0 +1,21 @@
+function [x_section, y_section, z_section, c_section, twist_section, S] = geometry_function(x_1, y_kink, dihedral)
+    kink_percentage = y_kink/(x_1(3));
+    c_kink = x_1(1) - kink_percentage*(x_1(1) - x_1(2));
+    twist_kink = x_1(6) - kink_percentage*(x_1(6) - x_1(7));
+    x_kink = sind(x_1(4))*y_kink;
+    z_kink = sind(dihedral)*y_kink;
+    
+    y_tip = x_1(3);
+    x_tip = x_kink + sind(x_1(5))*(y_tip - y_kink);
+    z_tip = sind(dihedral)*y_tip;
+    
+    x_section = [0, x_kink, x_tip];
+    y_section = [0, y_kink, y_tip];
+    z_section = [0, z_kink, z_tip];
+    c_section = [x_1(1), c_kink, x_1(2)];
+    twist_section = [x_1(6), twist_kink, x_1(7)];
+    
+    S1 = y_kink*(x_1(1) + c_kink)/2;
+    S2 = (y_tip - y_kink)*(x_1(2) + c_kink)/2;
+    S = 2*(S1 + S2);
+end
